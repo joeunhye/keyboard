260a2495a74ef1741452808b6c4d1c3875b0e889
joeunhye <masimel33@gmail.com>
2022-11-20 16:14:27 +0900

    mouse event add

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ class keyboard {
 	#keyboardEl;
 	#inputGroupEl;
 	#inputEl;
+	#keyPress = false;
+	#mouseDown = false;
 	constructor() {
 		this.#assignElement();
 		this.#addEvent();
@@ -23,6 +25,8 @@ class keyboard {
 		document.addEventListener("keydown", this.#onKeyDown);
 		document.addEventListener("keyup", this.#onKeyUp);
 		this.#inputEl.addEventListener("input", this.#onInput);
+		this.#keyboardEl.addEventListener("mousedown", this.#onMouseDown);
+		this.#keyboardEl.addEventListener("mouseup", this.#onMouseUp);
 	}
 	#onChangeTheme(e) {
 		document.documentElement.setAttribute("theme", e.target.checked ? "dark-mode" : "");
@@ -31,6 +35,8 @@ class keyboard {
 		document.body.style.fontFamily = e.target.value;
 	}
 	#onKeyDown = e => {
+		if (this.#mouseDown) return;
+		this.#keyPress = true;
 		if (e.isComposing || e.keyCode === 229 || /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(e.key)) {
 			//한글 입력
 			this.#inputGroupEl.classList.add("error");
@@ -40,13 +46,37 @@ class keyboard {
 			this.#keyboardEl.querySelector(`[data-code=${e.code}]`)?.classList.add("active");
 		}
 	};
+	#onKeyUp = e => {
+		if (this.#mouseDown) return;
+		this.#keyPress = false;
+		this.#keyboardEl.querySelector(`[data-code=${e.code}]`)?.classList.remove("active");
+	};
 	#onInput = e => {
 		if (e.isComposing || e.keyCode === 229 || /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(e.key)) {
 			e.target.value = e.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
 		}
 	};
-	#onKeyUp = e => {
-		this.#keyboardEl.querySelector(`[data-code=${e.code}]`)?.classList.remove("active");
+	#onMouseDown = e => {
+		if (this.#keyPress) return;
+		this.#mouseDown = true;
+		e.target.closest("div.key")?.classList.add("active");
+	};
+	#onMouseUp = e => {
+		if (this.#keyPress) return;
+		this.#mouseDown = false;
+		const keyEl = e.target.closest("div.key");
+		const isActive = !!keyEl?.classList.contains("active");
+		const val = keyEl?.dataset.val;
+		this.#keyboardEl.querySelector(".active")?.classList.remove("active");
+		if (isActive && !!val && val !== "Space" && val !== "Backspace") {
+			this.#inputEl.value += val;
+		}
+		if (isActive && val === "Space") {
+			this.#inputEl.value += " ";
+		}
+		if (isActive && val === "Backspace") {
+			this.#inputEl.value = this.#inputEl.value.slice(0, -1);
+		}
 	};
 }
 
